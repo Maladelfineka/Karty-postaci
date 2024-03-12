5ca34ef0c56dc0dc12b9994f0efa1df8a8f5e6d2
Ola <129540485+Maladelfineka@users.noreply.github.com>
2024-03-12 17:33:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -21,25 +21,78 @@ function karty(charactery){
 };
 var next = '';
 var nextButton = document.querySelector(".next-js");
+var next1 = '';
+var nextButton1= document.querySelector(".next-js1");
 
+var prev = '';
+var prevButton = document.querySelector(".prev-js");
+var prev1 = '';
+var prevButton1 = document.querySelector(".prev-js1");
+
+prevButton.disabled = true;
+prevButton1.disabled = true;
 let root = document.querySelector('.root');
+
 nextButton.onclick = function(){
    console.log("dziala")
+   prevButton.disabled= false;
    fetch(next)
       .then(response => response.json())
       .then(result => {
+         console.log(result)
          next = result.info.next;
+         prev = result.info.prev;
          karty(result.results);
          })
 }
 
+nextButton1.onclick = function(){
+   console.log("dziala")
+   prevButton.disabled= false;
+   fetch(next)
+      .then(response => response.json())
+      .then(result => {
+         console.log(result)
+         next = result.info.next;
+         prev = result.info.prev;
+         karty(result.results);
+         })
+}
 
+prevButton.onclick = function(){
+   
+   console.log("YeS!")
+   fetch(prev)
+      .then(response => response.json())
+      .then(result => {
+         
+         prev = result.info.prev;
+         
+         next1 = result.info.next;
+         karty(result.results);
+   })
+}
+prevButton1.onclick = function(){
+   
+   console.log("YeS!")
+   fetch(prev)
+      .then(response => response.json())
+      .then(result => {
+         
+         prev1 = result.info.prev;
+         
+         next = result.info.next;
+         karty(result.results);
+   })
+}
 
  
  fetch("https://rickandmortyapi.com/api/character")
  .then(response => response.json())
  .then(result => {
+      console.log(result)
       next = result.info.next;
+      prev = result.info.prev
     karty(result.results)
 
  });
